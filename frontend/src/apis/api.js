d40e97ab32bdfc9498989e44bfe202ab8a10667c
Marius Žakevičius <marius.zakevicius@alchemetrics.co.uk>
2020-01-14 20:19:17 +0200

    Allow Origin header

--- frontend/src/apis/api.js
+++ frontend/src/apis/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
-const heroku = 'https://logway1-app.herokuapp.com/api/';
-const local = 'http://localhost:8000/api';
+const heroku = "https://logway1-app.herokuapp.com/api/";
+const local = "http://localhost:8000/api";
 
 export default axios.create({
-    baseURL: local
+  baseURL: heroku
 });
